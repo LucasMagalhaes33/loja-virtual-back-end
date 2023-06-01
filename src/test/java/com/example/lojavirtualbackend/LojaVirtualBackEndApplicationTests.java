14e01cea37d10c326a839b1be9cda6208ede4036
lucasmagalhaes33 <lucas.magalhaes911@gmail.com>
2023-05-31 21:44:55 -0300

    adicionando maven ao projeto

---
+++ src/test/java/com/example/lojavirtualbackend/LojaVirtualBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lojavirtualbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
